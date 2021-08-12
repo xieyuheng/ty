403a9597ce0accb5930afd60d7834ba5f6346d63
Xie Yuheng <xyheme@gmail.com>
2021-08-12 17:43:47 +0800

    up

--- TODO.md
+++ TODO.md
@@ -1,8 +1,8 @@
+- `schemas/email` -- use json-schema -- subtype of `schemas/string`
+- `schemas/uri` -- use json-schema -- subtype of `schemas/string`
+
 - `schemas/union`
 - `schemas/intersection`
 
 - `schemas/const`
 - `schemas/enum`
-
-- `schemas/email` -- use json-schema -- subtype of `schemas/string`
-- `schemas/uri` -- use json-schema -- subtype of `schemas/string`
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,23 @@ import * as Schemas from "./schemas"
 export default {
   // NOTE primitive
   string: Schemas.StringSchema.create,
-  // email: Schemas.EmailSchema.create, // NOTE subtype of `string`
-  // uri: Schemas.UriSchema.create, // NOTE subtype of `string`
+  // email: Schemas.EmailSchema.create, // <: string
+  // uri: Schemas.UriSchema.create, // <: string
   number: Schemas.NumberSchema.create,
-  int: Schemas.IntSchema.create, // NOTE subtype of `number`
+  int: Schemas.IntSchema.create, // <: number
   null: Schemas.NullSchema.create,
   undefined: Schemas.UndefinedSchema.create,
   boolean: Schemas.BooleanSchema.create,
   any: Schemas.AnySchema.create,
+
   // NOTE compound
   object: Schemas.ObjectSchema.create,
   array: Schemas.ArraySchema.create,
   dict: Schemas.DictSchema.create,
+
+  // NOTE set theory
+  // const: Schemas.ConstSchema.create,
+  // enum: Schemas.EnumSchema.create,
+  // union: Schemas.UnionSchema.create,
+  // intersection: Schemas.IntersectionSchema.create,
 }
