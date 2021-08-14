d815df8317d972c1a8aad0d1b0c62a7aad5a3ec0
Xie Yuheng <xyheme@gmail.com>
2021-08-14 17:12:00 +0800

    up

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,3 @@
-import * as Schemas from "./schemas"
+export { default } from "./api"
 
-export default {
-  // NOTE primitive
-  string: Schemas.StringSchema.create,
-  format: Schemas.FormatSchema.create, // <: string
-  email: (constraints: Schemas.StringConstraints = {}) =>
-    Schemas.FormatSchema.create("email", constraints),
-  uri: (constraints: Schemas.StringConstraints = {}) =>
-    Schemas.FormatSchema.create("uri", constraints),
-  date: (constraints: Schemas.StringConstraints = {}) =>
-    Schemas.FormatSchema.create("date", constraints),
-  time: (constraints: Schemas.StringConstraints = {}) =>
-    Schemas.FormatSchema.create("time", constraints),
-  number: Schemas.NumberSchema.create,
-  int: Schemas.IntSchema.create, // <: number
-  null: Schemas.NullSchema.create,
-  undefined: Schemas.UndefinedSchema.create,
-  boolean: Schemas.BooleanSchema.create,
-  any: Schemas.AnySchema.create,
-
-  // NOTE compound
-  object: Schemas.ObjectSchema.create,
-  array: Schemas.ArraySchema.create,
-  dict: Schemas.DictSchema.create,
-
-  // NOTE set theory
-  same: Schemas.SameSchema.create,
-  union: Schemas.UnionSchema.create,
-  intersection: Schemas.IntersectionSchema.create,
-
-  // NOTE structural
-  omit: Schemas.OmitSchema.create,
-}
+export * from "./schema"
