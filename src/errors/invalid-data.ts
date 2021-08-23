8c282ecbfe3a3ad64cd2d511707a16b5a025c04b
Xie Yuheng <xyheme@gmail.com>
2021-08-23 21:47:14 +0800

    up

--- src/errors/invalid-data.ts
+++ src/errors/invalid-data.ts
@@ -1,6 +1,4 @@
 export class InvalidData extends Error {
-  private instanceofInvalidData = "ty"
-
   data: any
   msg: string
   keys: Array<string | number> = []
@@ -17,6 +15,15 @@ export class InvalidData extends Error {
     this.msg = opts.msg
     this.keys = opts.keys || []
   }
+
+  // NOTE We can not use `instanceof` to check whether an error is `InvalidData`,
+  //   because `ty` might be imported by different clients,
+  //   and the schema written by them might be composed together.
+  // When this happened, different references to the `InvalidData` class,
+  //   will not be viewed as the same by `instanceof`.
+  // Thus we need to write our type guard.
+
+  private instanceofInvalidData = "ty"
 
   static guard(error: any): error is InvalidData {
     return error["instanceofInvalidData"] === "ty"
